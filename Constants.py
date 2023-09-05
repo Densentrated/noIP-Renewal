85d2d2a39fce4a2924d26f7a3d87d02235c482ba
Densentrated <72484574+Densentrated@users.noreply.github.com>
2023-09-05 00:49:19 -0700

    created working headed version, need to create working headless version

--- Constants.py
+++ Constants.py
@@ -1,7 +1,7 @@
 
-USERNAME = "densentrated"
-USERPASSWORD = "ttFqN9scSYTq8o"
+USERNAME = "berryrelman459@gmail.com"
+USERPASSWORD = "berryrelman459@gmail.com"
 DOMAINNAME = "fieldofflowers.ddns.net"
-WEBSITEURL = "http://www.noip.com/sign-up"
+WEBSITEURL = "https://www.noip.com/login"
 #Interval in months
 INTERVAL = "6"
--- Script.py
+++ Script.py
@@ -1,10 +1,39 @@
-import Constants, os, selenium, time
-
+import Constants
+import selenium
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+import time
 
+#gets browser and opens website
 browser = webdriver.Chrome(ChromeDriverManager().install())
-noIPWebsite = browser.get(Constants.WEBSITEURL)
+browser.get(Constants.WEBSITEURL)
 
-noIPWebsite.find_element(By.XPATH, '//*[@id="email"]').send_keys(Constants.USERNAME)
+# gets through the login page
+# page elements
+emailBox = browser.find_element(By.XPATH, "/html/body/section[3]/section/div/div/div/div/div/div[2]/form/section[1]/input")
+passwordBox = browser.find_element(By.XPATH, "/html/body/section[3]/section/div/div/div/div/div/div[2]/form/section[2]/input")
+loginButton = browser.find_element(By.XPATH, "/html/body/section[3]/section/div/div/div/div/div/div[2]/form/button")
+
+if emailBox != None and passwordBox != None:
+    print("Found email and password boxes, entering info...")
+
+emailBox.send_keys(Constants.USERNAME)
+passwordBox.send_keys(Constants.USERPASSWORD)
+loginButton.click()
+
+# gets to hostname menu
+time.sleep(2)
+browser.get("https://my.noip.com/dynamic-dns?mode=edit&id=84780181")
+
+#clicks cancel button
+time.sleep(1)
+cancelButton = browser.find_element(By.XPATH, "/html/body/div[1]/div/div/div[3]/div[4]/div/div/div/div[4]/button[2]")
+cancelButton.click()
+# clicks confirm button
+time.sleep(1)
+confirmButton = browser.find_element(By.XPATH, "/html/body/div[1]/div/div/div[3]/div[1]/div[2]/div/div/div[2]/div[1]/table/tbody/tr/td[5]/button[1]")
+confirmButton.click()
+
+browser.close()
+
